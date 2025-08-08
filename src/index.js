2b1582e3ed1df2453771f2d044cdf523f69fa5b0
Sauvik Das <sauvik.sgc2016@gmail.com>
2025-08-08 21:47:25 +0530

    Bug Fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.PORT || 8000, () => {
+        app.listen(process.env.PORT, () => {
             console.log(`App Listening on Port ${process.env.PORT}`);
 
             app.get('/', (req, res) => {
